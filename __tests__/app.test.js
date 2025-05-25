63e581e54dad55cc8f080e1cec291bc87ba92c20
Liran Tal <liran.tal@gmail.com>
2025-05-25 19:44:21 +0300

    chore: clean up boilerplate code tests

--- __tests__/app.test.js
+++
@@ -1,8 +0,0 @@
-import { test, describe } from 'node:test'
-import assert from 'node:assert'
-
-describe('Node.js API Documentation Server', () => {
-  test('Server should start successfully', async () => {
-    // Your test code here
-  })
-})
